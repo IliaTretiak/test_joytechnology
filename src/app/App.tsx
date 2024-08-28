a59bc62986cee4262459abfa1ea721003b7ce82b
Ilia Tretiak <nba-91@mail.ru>
2024-08-28 16:52:05 +0400

    init zustand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,20 @@
 "use client"
-import { CounterStoreProvider } from '../providers/store-provider'
 import Header from '../components/header/index'
 import Bubble from '../components/messager/index'
 import Footer from '../components/footer'
 
+import CounterComponent from '../components/test/CounterControls';
+
 const Page = () => {
     return (
-      <CounterStoreProvider>
       <div className='flex flex-col h-[100vw]'>
             <Header />
             <Bubble />
             <div className='flex items-end'>
             <Footer />
             </div>
+            <CounterComponent />
       </div>
-      </CounterStoreProvider>
     )
 }
 
