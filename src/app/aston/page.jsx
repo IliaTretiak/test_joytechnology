6e2fa23731223297e0da36995a71175fd479a9e3
Ilia Tretiak <nba-91@mail.ru>
2024-11-12 09:47:54 +0400

    init

--- src/app/aston/page.jsx
+++ src/app/aston/page.jsx
@@ -1,8 +1,7 @@
 const Page = () => {
     return (
         <div className="w-[200px] h-[200px] bg-black">
-            <div className="w-[50px] h-[50px0] m-[auto] bg-white">
-                123
+            <div className="w-[50px] h-[50px] m-[auto] bg-white">
             </div>
         </div>
     )
